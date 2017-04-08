4760dbf5310854b4613581ef54c5f2e5ca1cebb7
Charleezy <charleezyguan@gmail.com>
2017-04-08 15:46:47 -0400

    Removed unnecessary phpmailer and made send.php accurate

--- php/send.php
+++ php/send.php
@@ -1,9 +1,15 @@
 <?php
-require 'PHPMailer-master/PHPMailerAutoload.php';
+require 'phpmailer/PHPMailerAutoload.php';
 
 $mail = new PHPMailer(true);
 
 $mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$name     = $_POST['name'];
+$companyname     = $_POST['companyname'];
+$email    = $_POST['email'];
+$tel    = $_POST['tel'];
+$comments = $_POST['comments'];
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'lv-shared04.cpanelplatform.com';  // Specify main and backup SMTP servers
@@ -13,19 +19,19 @@ $mail->Password = 'qR([72;yJ+(E';                           // SMTP password
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
 
-$mail->setFrom('test@carolinajan.com', 'Charlie');
+$mail->setFrom('test@carolinajan.com', $name);
 $mail->addAddress('hello@carolinajan.com', 'Carolini Jani');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
-$mail->addReplyTo('DoctorProfessor@charlie.com', 'Charlie');
-$mail->addCC('cc@example.com');
-$mail->addBCC('bcc@example.com');
+$mail->addReplyTo($email, $name);
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
 
 //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
 //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 $mail->isHTML(true);                                  // Set email format to HTML
 
-$mail->Subject = 'Here is the subject';
-$mail->Body    = 'My name is Charlie and I am a Doctor Professor and my girlfriend is sooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo *gasps for breath* ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo smart hardworking and sexy ;)';
+$mail->Subject = "New message from {$name}";
+$mail->Body    = $comments;
 $mail->AltBody = '';
 
 if(!$mail->send()) {
